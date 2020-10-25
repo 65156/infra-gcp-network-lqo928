f7a8559f6d791a212ba5f17210028ab3983e1f29
Fraser Elliot Carter Smith <fraser.carter@gmail.com>
2020-10-26 10:32:11 +1100

    added kubernetes network

--- _variables.tf
+++ _variables.tf
@@ -12,32 +12,32 @@ variable "global" {
 variable "subnet_cidr_dev" {
   type        = list
   description = "subnets to provision"
-  default     = ["10.32.0.0/21", "10.32.32.0/21"]
+  default     = ["10.32.0.0/21", "10.32.32.0/21", "10.32.8.0/21"]
 }
 variable "subnet_cidr_stage" {
   type        = list
   description = "subnets to provision"
-  default     = ["10.40.0.0/21", "10.40.32.0/21"]
+  default     = ["10.40.0.0/21", "10.40.32.0/21", "10.40.8.0/21"]
 }
 variable "subnet_cidr_prod" {
   type        = list
   description = "subnets to provision"
-  default     = ["10.48.0.0/21", "10.48.32.0/21"]
+  default     = ["10.48.0.0/21", "10.48.32.0/21", "10.48.8.0/21"]
 }
 variable "subnet_cidr_mgmt" {
   type        = list
   description = "subnets to provision"
-  default     = ["10.61.0.0/21", "10.61.32.0/21"]
+  default     = ["10.61.0.0/21", "10.61.32.0/21","10.61.8.0/21"]
 }
 variable "subnet_names" {
   type        = list
   description = "List of subnet names"
-  default     = ["default", "default"] #,"block-b",kubernetes-a","kubernetes-b"]
+  default     = ["default", "default", "kubernetes"] #,"block-b",kubernetes-a","kubernetes-b"]
 }
 variable "subnet_region" {
   type        = list
   description = "List of subnet regions"
-  default     = ["australia-southeast1", "us-west2"]
+  default     = ["australia-southeast1", "us-west2", "australia-southeast1"]
 }
 variable "subnet_private_ip_google_access" {
   default = true
